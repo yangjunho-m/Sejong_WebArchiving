13b15dc86eeb5bf83ba931c3aa066469285a44c5
양준호 <dlqfur89@gmail.com>
2026-09-13 19:23:18 +0900

    프로덕트 버전마무리

--- Web/start-server.bat
+++ Web/start-server.bat
@@ -1,3 +1,12 @@
 @echo off
 cd /d "%~dp0"
-py -m http.server 8080
+echo.
+echo Starting local server...
+echo Open: http://localhost:8080/
+start "" /b py -m http.server 8080
+timeout /t 2 /nobreak > nul
+start "" http://localhost:8080/
+echo.
+echo The site is running at http://localhost:8080/
+echo Keep this window open while you use the site.
+pause
